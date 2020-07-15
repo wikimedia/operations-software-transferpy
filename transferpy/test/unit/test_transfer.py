b062e81783d71e85523d4de2822de6fedd0fd95d
Privacybatm <ajupazhamayil@gmail.com>
2020-07-15 23:08:52 +0530

    Transferer.py: Add unit tests
    This patch adds unit-tests for the following functions/scenarios:
    
    - host_exists (test_host_exists)
    - is_socket (test_is_socket)
    - dir_is_empty (test_dir_is_empty)
    - parallel_checksum_source_command (test_parallel_checksum_source_command)
    - parallel_checksum_target_command (test_parallel_checksum_target_command)
    - read_checksum (test_read_checksum)
    - netcat_send_command and listen command (test_netcat_send_command, test_netcat_listen_command)
    - tar and untar command (test_untar_command, test_tar_command)
    - get_datadir_from_socket (test_get_datadir_from_socket)
    - xtrabackup_command (test_xtrabackup_command)
    - mbstream_command  (test_mbstream_command)
    - password function (test_password)
    - copy_to as a whole (test_copy_to_success, test_copy_to_failure)
    - sanity_checks (test_sanity_checks_xtrabackup, test_sanity_checks_file)
    - after_transfer_checks (test_after_transfer_checks)
    
    Bug: T257600
    Change-Id: If21db2ee3c8f8aa38540d3f2581f4d7bf7e55177

--- transferpy/test/unit/test_transfer.py
+++ transferpy/test/unit/test_transfer.py
@@ -242,6 +242,231 @@ class TestTransferer(unittest.TestCase):
             mocked_close_firewall.assert_called_once_with('source', 4400)
             self.assertTrue(type(command) == list)
 
+    def test_copy_to_success(self):
+        """Test case for the successful run of Transferer.copy_to function"""
+        self.options['compress'] = False
+        self.options['parallel_checksum'] = False
+        self.options['encrypt'] = False
+        self.options['port'] = 4400
+        with patch('transferpy.Transferer.time'):
+            target_host = 'target'
+            target_path = 'target_path'
+            self.executor.run.return_value.returncode = 0
+            returncode = self.transferer.copy_to(target_host, target_path)
+            self.executor.start_job.assert_called_once()
+            # Successful run should call the wait_job function
+            self.executor.wait_job.assert_called_once()
+            self.executor.kill_job.assert_not_called()
+            self.assertEqual(returncode, 0)
+
+    def test_copy_to_failure(self):
+        """Test case for the failed run of Transferer.copy_to function"""
+        self.options['compress'] = False
+        self.options['parallel_checksum'] = False
+        self.options['encrypt'] = False
+        self.options['port'] = 4400
+        with patch('transferpy.Transferer.time'):
+            target_host = 'target'
+            target_path = 'target_path'
+            self.executor.run.return_value.returncode = 1
+            returncode = self.transferer.copy_to(target_host, target_path)
+            self.executor.start_job.assert_called_once()
+            # Failure should call the kill_job function
+            self.executor.kill_job.assert_called_once()
+            self.executor.wait_job.assert_not_called()
+            self.assertEqual(returncode, 1)
+
+    def test_is_socket(self):
+        """Test is_socket"""
+        path = 'path'
+        command = ['/bin/bash', '-c', r'"[ -S "{}" ]"'.format(path)]
+        self.transferer.is_socket('source', path)
+        self.executor.run.assert_called_once_with('source', command)
+
+    def test_host_exists(self):
+        """Test host_exists"""
+        command = ['/bin/true']
+        self.transferer.host_exists('source')
+        self.executor.run.assert_called_once_with('source', command)
+
+    def test_dir_is_empty(self):
+        """Test dir_is_empty"""
+        directory = 'dir'
+        command = ['/bin/bash', '-c', r'"[ -z \"$(/bin/ls -A {})\" ]"'.format(directory)]
+        self.transferer.dir_is_empty(directory, 'source')
+        self.executor.run.assert_called_once_with('source', command)
+
+    def test_parallel_checksum_source_command(self):
+        """Test parallel_checksum_source_command"""
+        self.options['parallel_checksum'] = True
+        checksum_command = '| tee >(md5sum > {})'.format(self.transferer.parallel_checksum_source_path)
+        self.assertEqual(checksum_command, self.transferer.parallel_checksum_source_command)
+        # Make parallel_checksum False and try again
+        self.options['parallel_checksum'] = False
+        self.assertEqual('', self.transferer.parallel_checksum_source_command)
+
+    def test_parallel_checksum_target_command(self):
+        """Test parallel_checksum_target_command"""
+        self.options['parallel_checksum'] = True
+        checksum_command = '| tee >(md5sum > {})'.format(self.transferer.parallel_checksum_target_path)
+        self.assertEqual(checksum_command, self.transferer.parallel_checksum_target_command)
+        # Make parallel_checksum False and try again
+        self.options['parallel_checksum'] = False
+        self.assertEqual('', self.transferer.parallel_checksum_target_command)
+
+    def test_read_checksum(self):
+        """Test read_checksum"""
+        path = 'path'
+        command = ['/bin/bash', '-c', '/bin/cat < {} && /bin/rm {}'.format(path, path)]
+        self.executor.run.return_value.returncode = 0
+        self.executor.run.return_value.stdout = "checksum - path"
+        checksum = self.transferer.read_checksum('source', path)
+        self.executor.run.assert_called_once_with('source', command)
+        self.assertEqual(checksum, "checksum - path")
+
+    def test_netcat_send_command(self):
+        """Test netcat_send_command"""
+        target_host = 'source'
+        self.options['port'] = 4400
+        expect_command = '| /bin/nc -q 0 -w 300 {} {}'.format(target_host, self.options['port'])
+        actual_command = self.transferer.netcat_send_command(target_host)
+        self.assertEqual(expect_command, actual_command)
+
+    def test_netcat_listen_command(self):
+        """Test netcat_listen_command"""
+        self.options['port'] = 4400
+        expect_command = '/bin/nc -l -w 300 -p {}'.format(self.options['port'])
+        actual_command = self.transferer.netcat_listen_command
+        self.assertEqual(expect_command, actual_command)
+
+    def test_tar_command(self):
+        """Test tar_command"""
+        expected_command = '/bin/tar cf -'
+        actual_command = self.transferer.tar_command
+        self.assertEqual(expected_command, actual_command)
+
+    def test_untar_command(self):
+        """Test untar_command"""
+        expected_command_decompress = '| /bin/tar --strip-components=1 -xf -'
+        expected_command_file = '| /bin/tar xf -'
+        self.options['type'] = 'decompress'
+        actual_command = self.transferer.untar_command
+        self.assertEqual(actual_command, expected_command_decompress)
+        self.options['type'] = 'file'
+        actual_command = self.transferer.untar_command
+        self.assertEqual(actual_command, expected_command_file)
+
+    def test_get_datadir_from_socket(self):
+        """Test get_datadir_from_socket"""
+        socket = 'mysqld.sock'
+        datadir = '/srv/sqldata'
+        actual_dir = self.transferer.get_datadir_from_socket(socket)
+        self.assertEqual(datadir, actual_dir)
+        socket = 'test.mysqld.s1.sock'
+        datadir = '/srv/sqldata.s1'
+        actual_dir = self.transferer.get_datadir_from_socket(socket)
+        self.assertEqual(datadir, actual_dir)
+        # Give wrong socket input
+        socket = 'test.mysqld.smx1.sock'
+        with self.assertRaises(Exception):
+            self.transferer.get_datadir_from_socket(socket)
+
+    def test_xtrabackup_command(self):
+        """Test xtrabackup_command"""
+        self.transferer.source_path = 'mysqld.sock'
+        socket = self.transferer.source_path
+        datadir = self.transferer.get_datadir_from_socket(socket)
+        expected_command = 'xtrabackup --backup --target-dir /tmp ' \
+                           '--user {} --socket={} --close-files --datadir={} --parallel={} ' \
+                           '--stream=xbstream --slave-info --skip-ssl'.\
+            format('root', socket, datadir, 16)
+        actual_command = self.transferer.xtrabackup_command
+        self.assertEqual(expected_command, actual_command)
+
+    def test_mbstream_command(self):
+        """Test mbstream_command"""
+        expected_command = '| mbstream -x'
+        actual_command = self.transferer.mbstream_command
+        self.assertEqual(expected_command, actual_command)
+
+    def test_password(self):
+        """Test password function"""
+        self.transferer._password = None
+        password = self.transferer.password
+        self.assertNotEqual(password, None)
+        self.transferer._password = 'password'
+        password = self.transferer.password
+        self.assertEqual('password', password)
+
+    def test_sanity_checks_file(self):
+        """Test sanity_checks for file/dir"""
+        with patch.object(Transferer, 'host_exists') as mocked_host_exists, \
+                patch.object(Transferer, 'disk_usage') as mocked_disk_usage, \
+                patch.object(Transferer, 'file_exists') as mocked_file_exists, \
+                patch.object(Transferer, 'has_available_disk_space') as mocked_disk_space, \
+                patch.object(Transferer, 'is_socket') as mocked_is_socket:
+            self.transferer.target_hosts = ['target']
+            self.transferer.target_paths = ['path']
+            self.options['checksum'] = True
+            self.options['type'] = 'file'
+            mocked_disk_space.return_value = True
+            mocked_is_socket.return_value = False
+            mocked_file_exists.side_effect = [True, True, False]
+            mocked_host_exists.return_value.returncode = 0
+            self.transferer.sanity_checks()
+            self.assertEqual(mocked_host_exists.call_count, 2)
+            mocked_disk_space.assert_called_once()
+            mocked_disk_usage.assert_called_once()
+            self.assertEqual(mocked_file_exists.call_count, 3)
+
+    def test_sanity_checks_xtrabackup(self):
+        """Test sanity_checks for xtrabackup/decompress"""
+        with patch.object(Transferer, 'host_exists') as mocked_host_exists, \
+                patch.object(Transferer, 'disk_usage') as mocked_disk_usage, \
+                patch.object(Transferer, 'file_exists') as mocked_file_exists, \
+                patch.object(Transferer, 'dir_is_empty') as mocked_dir_is_empty, \
+                patch.object(Transferer, 'has_available_disk_space') as mocked_disk_space, \
+                patch.object(Transferer, 'is_socket') as mocked_is_socket:
+            self.transferer.target_hosts = ['target']
+            self.transferer.target_paths = ['path']
+            self.options['checksum'] = True
+            self.options['type'] = 'xtrabackup'
+            mocked_dir_is_empty.return_value = True
+            mocked_disk_space.return_value = True
+            mocked_is_socket.return_value = True
+            mocked_file_exists.return_value = True
+            mocked_host_exists.return_value.returncode = 0
+            self.transferer.sanity_checks()
+            mocked_dir_is_empty.assert_called_once()
+            self.assertEqual(mocked_host_exists.call_count, 2)
+            mocked_disk_space.assert_called_once()
+            mocked_disk_usage.assert_called_once()
+            mocked_is_socket.assert_called_once()
+            self.assertEqual(mocked_file_exists.call_count, 2)
+
+    def test_after_transfer_checks(self):
+        """Test after_transfer_checks"""
+        with patch.object(Transferer, 'disk_usage') as mocked_disk_usage, \
+                patch.object(Transferer, 'file_exists') as mocked_file_exists, \
+                patch.object(Transferer, 'calculate_checksum') as mocked_calculate_checksum, \
+                patch.object(Transferer, 'read_checksum') as mocked_read_checksum, \
+                patch.object(Transferer, 'remove_temp_paths') as mocked_remove_temp_paths:
+            target_host = 'target'
+            target_path = 'path'
+            self.options['checksum'] = True
+            self.options['parallel_checksum'] = True
+            self.options['type'] = 'file'
+            self.transferer.checksum = 'checksum'
+            mocked_calculate_checksum.return_value = 'checksum'
+            mocked_file_exists.return_value = True
+            result = self.transferer.after_transfer_checks(0, target_host, target_path)
+            mocked_disk_usage.assert_called_once()
+            mocked_calculate_checksum.assert_called_once()
+            mocked_file_exists.assert_called_once()
+            mocked_remove_temp_paths.assert_called_once()
+            self.assertEqual(mocked_read_checksum.call_count, 2)
+            self.assertEqual(result, 0)
+
 
 class TestFirewall(unittest.TestCase):
     """Test cases for Firewall module"""
